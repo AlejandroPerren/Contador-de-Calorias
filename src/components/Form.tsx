ed1663eb70d0bb5b62c3a4de88d3e0743a8a9a6b
Alejandro <aletomasperren@gmail.com>
2024-12-28 17:57:54 -0300

    useReducer And show Activities or foods

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,23 @@
-import {  useState, ChangeEvent, FormEvent } from "react";
+import {  useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
 import { Activity } from "../types";
+import { ActivityActions } from "../reducers/activity.reducer";
 
 
-export const Form = () => {
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: '',
-    calories: 0,
-  })
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>
+}
+
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: '',
+  calories: 0,
+}
+
+export const Form = ({dispatch}: FormProps) => {
+  const [activity, setActivity] = useState<Activity>(initialState)
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     const isNumberField = ['category', 'calories'].includes(e.target.id)
@@ -26,8 +35,16 @@ export const Form = () => {
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
   e.preventDefault()
-  
+
+  dispatch({type: "save-activity", payload: {newActivity: activity}})
+
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
   }
+
+
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg" onSubmit={handleSubmit}>
       <div className="grid grid-cols-1 gap-3">
